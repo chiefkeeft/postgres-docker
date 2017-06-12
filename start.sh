d158990457bbd55003c6e875cecadbe7c7edbca0
chiefkeeft <chiefkeeft@gmail.com>
2017-06-12 15:22:31 +0600

    Create start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ sudo systemctl daemon-reload
 sudo systemctl enable pgbouncer.service
 sudo service postgresql restart
 sudo chown -R postgres:postgres /var/log/postgresql/
-
+sudo service dbus start
